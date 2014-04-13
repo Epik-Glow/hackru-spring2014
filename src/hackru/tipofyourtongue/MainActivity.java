cf02ddadefbcb2ee2fb932f2e24b9e05f074926b
Richard Chu <richardchu10@gmail.com>
2014-04-13 00:09:29 -0400

    api call works

--- src/hackru/tipofyourtongue/MainActivity.java
+++ src/hackru/tipofyourtongue/MainActivity.java
@@ -3,32 +3,35 @@ package hackru.tipofyourtongue;
 import java.util.List;
 
 import com.google.android.glass.app.Card;
-import com.google.android.glass.timeline.TimelineManager;
-import com.temboo.Library.Wordnik.Words.ReverseDictionary;
-import com.temboo.Library.Wordnik.Words.ReverseDictionary.ReverseDictionaryInputSet;
-import com.temboo.Library.Wordnik.Words.ReverseDictionary.ReverseDictionaryResultSet;
-import com.temboo.core.TembooException;
-import com.temboo.core.TembooSession;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.util.Log;
-import android.view.LayoutInflater;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends Activity {
 
-	private boolean mReturningWithResult = false;
-	private String text;
-	private Intent data;
-
 	private static final int SPEECH_REQUEST = 0;
-	private Card card = new Card(this);
+	private Card card;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		card = new Card(this);
 
 		// Starts up a speech request
 		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
@@ -40,54 +43,57 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == SPEECH_REQUEST && resultCode == RESULT_OK) {
-			this.data = data;
-			mReturningWithResult = true;
+			// Gets the spoken text
+			List<String> results = data
+					.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+			String spokenText = results.get(0);
+			new ReverseDictionaryTask().execute(spokenText);
 		}
-
 		super.onActivityResult(requestCode, resultCode, data);
 	}
 
-	protected void onPostResume() {
-		super.onPostResume();
+	private class ReverseDictionaryTask extends AsyncTask<String, Void, String> {
 
-		if (mReturningWithResult) {
-			// Gets the spoken text
-			List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-			String spokenText = results.get(0);
-			text = spokenText;
-
-			// Gets the reverse dictionary output
-			TembooSession session = null;
-			ReverseDictionaryResultSet reverseDictionaryResults = null;
+		@Override
+		protected String doInBackground(String... input) {
+			String result = "Didn't work";
+			JSONObject json;
 			try {
-				session = new TembooSession("RichardChu", "GlassApp",
-						"f00de67c02a503b0850080e3e090b9e53bb68fa621377b3dd");
-
-				// Instantiate the Choreo, using a previously instantiated
-				// TembooSession object
-				ReverseDictionary reverseDictionaryChoreo = new ReverseDictionary(
-						session);
-
-				// Get an InputSet object for the choreo
-				ReverseDictionaryInputSet reverseDictionaryInputs = reverseDictionaryChoreo
-						.newInputSet();
-
-				// Set inputs
-				reverseDictionaryInputs
-						.set_APIKey("f00de67c02a503b0850080e3e090b9e53bb68fa621377b3dd");
-				reverseDictionaryInputs.set_Query(text);
-
-				// Execute Choreo
-				reverseDictionaryResults = reverseDictionaryChoreo
-						.execute(reverseDictionaryInputs);
-
-				// Sets the card text
-				card.setText(reverseDictionaryResults.get_Response());
+				json = readJsonFromUrl("http://api.wordnik.com:80/v4/words.json/reverseDictionary?query=hard%20to%20find&minCorpusCount=5&maxCorpusCount=-1&minLength=1&maxLength=-1&includeTags=false&skip=0&limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5");
+				result = json.toString();
 			} catch (Exception e) {
-				Log.e("ERROR", e.getMessage());
-				card.setText("Doesn't work");
+				Log.e("TipOfYourTongue", e.getMessage());
 			}
+			
+			return result;
 		}
-		mReturningWithResult = false;
+
+		@Override
+		protected void onPostExecute(String result) {
+			// Sets the card text
+			card.setText(result);
+			setContentView(card.toView());
+		}
+		
+		private String readAll(Reader rd) throws IOException {
+		    StringBuilder sb = new StringBuilder();
+		    int cp;
+		    while ((cp = rd.read()) != -1) {
+		      sb.append((char) cp);
+		    }
+		    return sb.toString();
+		  }
+		
+		public JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+		    InputStream is = new URL(url).openStream();
+		    try {
+		      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+		      String jsonText = readAll(rd);
+		      JSONObject json = new JSONObject(jsonText);
+		      return json;
+		    } finally {
+		      is.close();
+		    }
+		  }
 	}
 }
