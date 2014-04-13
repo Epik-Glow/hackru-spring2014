920820fe53ef35f1cd16794b48d802bfaba06ae5
Daven Wu <EpikGlow@Gmail.com>
2014-04-13 01:45:23 -0400

    Added scrolling cards for words

--- src/hackru/tipofyourtongue/MainActivity.java
+++ src/hackru/tipofyourtongue/MainActivity.java
@@ -1,15 +1,21 @@
 package hackru.tipofyourtongue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.android.glass.app.Card;
+import com.google.android.glass.widget.CardScrollAdapter;
+import com.google.android.glass.widget.CardScrollView;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -25,19 +31,27 @@ import org.json.JSONObject;
 public class MainActivity extends Activity {
 
 	private static final int SPEECH_REQUEST = 0;
-	private Card card;
+	private ArrayList<Card> cards;
+	private CardScrollView cardScrollView;
+	private CardScrollViewAdapter cardScrollAdapter;
+	private Context context;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		context = this;
 
-		card = new Card(this);
+		cards = new ArrayList<Card>();
+		cardScrollAdapter = new CardScrollViewAdapter();
+		cardScrollView = new CardScrollView(this);
+		cardScrollView.setAdapter(cardScrollAdapter);
+		cardScrollView.activate();
 
 		// Starts up a speech request
 		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 		startActivityForResult(intent, SPEECH_REQUEST);
 
-		setContentView(card.toView());
+		setContentView(cardScrollView);
 	}
 
 	@Override
@@ -52,21 +66,48 @@ public class MainActivity extends Activity {
 		}
 		super.onActivityResult(requestCode, resultCode, data);
 	}
-
-	private class ReverseDictionaryTask extends AsyncTask<String, Void, String> {
+	
+	private class CardScrollViewAdapter extends CardScrollAdapter {
+		
+		@Override
+		public int findIdPosition(Object id) {
+			return -1;
+		}
 
 		@Override
-		protected String doInBackground(String... input) {
-			String result = "";
-			String words = "";
+		public int findItemPosition(Object item) {
+			return cards.indexOf(item);
+		}
+
+		@Override
+		public int getCount() {
+			return cards.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return cards.get(position);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			return cards.get(position).toView();
+		}
+		
+	}
+
+	private class ReverseDictionaryTask extends AsyncTask<String, Void, ArrayList<String>> {
+
+		@Override
+		protected ArrayList<String> doInBackground(String... input) {
+			ArrayList<String> words = new ArrayList<String>();
 			JSONObject json;
 			try {
 				json = readJsonFromUrl("http://api.wordnik.com:80/v4/words.json/reverseDictionary?query="+input[0]+"&minCorpusCount=5&maxCorpusCount=-1&minLength=1&maxLength=-1&includeTags=false&skip=0&limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5");
 				
 				int numOfResults = json.getInt("totalResults");
-				words = "";
 				for (int i = 0; i < numOfResults; i++){
-					words += " " + json.getJSONArray("results").getJSONObject(i).getString("word");
+					words.add(json.getJSONArray("results").getJSONObject(i).getString("word"));
 				}
 			} catch (Exception e) {
 				Log.e("TipOfYourTongue", e.getMessage());
@@ -76,10 +117,14 @@ public class MainActivity extends Activity {
 		}
 
 		@Override
-		protected void onPostExecute(String result) {
+		protected void onPostExecute(ArrayList<String> result) {
 			// Sets the card text
-			card.setText(result);
-			setContentView(card.toView());
+			for(String word : result) {
+				Card card = new Card(context);
+				card.setText(word);
+				cards.add(card);
+			}
+			cardScrollAdapter.notifyDataSetChanged();
 		}
 		
 		private String readAll(Reader rd) throws IOException {
